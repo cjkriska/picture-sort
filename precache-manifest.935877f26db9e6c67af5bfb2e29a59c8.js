88c0dc1e3ba9a37f8e999f11a89191f5bdba5df2
Charles Kriska <charleskriska@gmail.com>
2020-09-30 20:35:17 -0500

    Updates

---
+++ precache-manifest.935877f26db9e6c67af5bfb2e29a59c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a935c8d7c56f3c60d46adbb74f476c",
+    "url": "/picture-sort/index.html"
+  },
+  {
+    "revision": "063eac6a0c5e740a0106",
+    "url": "/picture-sort/static/js/2.7b463675.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/picture-sort/static/js/2.7b463675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0aaacfc1a57e973e86",
+    "url": "/picture-sort/static/js/main.2f7efdc8.chunk.js"
+  },
+  {
+    "revision": "58150716c540c2e00aab",
+    "url": "/picture-sort/static/js/runtime-main.64552316.js"
+  }
+]);
